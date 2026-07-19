e2823f002bbc448654875f694fb3a12b489cfca6
sihoo <pelomon73@gmail.com>
2026-07-19 15:46:14 +0900

    submit

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, jsonify, request
+import os
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+load_dotenv()
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient(os.getenv("MONGODB_URI"))
 db = client.dbjungle  # 'dbjungle'라는 이름의 db를 만들거나 사용합니다.
 
 
